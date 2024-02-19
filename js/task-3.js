ddf8f727e9647f86ee0d0d10c98b5a04fb3f5dd4
FedorovaElizaveta <147994204+FedorovaElizaveta@users.noreply.github.com>
2024-02-20 01:59:33 +0200

    task-3 is fixed

--- js/task-3.js
+++ js/task-3.js
@@ -1,23 +1,25 @@
 class StringBuilder {
+  #value;
+
   constructor(initialValue) {
-    this.value = initialValue;
+    this.#value = initialValue;
   }
 
   getValue() {
-    return this.value;
+    return this.#value;
   }
 
   padEnd(str) {
-    this.value = this.value.concat(str);
+    this.#value = this.#value.concat(str);
   }
 
   padStart(str) {
-    this.value = str.concat(this.value);
+    this.#value = str.concat(this.#value);
   }
 
   padBoth(str) {
-    this.value = this.value.concat(str);
-    this.value = str.concat(this.value);
+    this.#value = this.#value.concat(str);
+    this.#value = str.concat(this.#value);
   }
 }
 
